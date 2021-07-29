75fa94720aaa9e475d3d8d89f372baa5c30277bc
Lais-Peixoto <laispeixotodemoraes@gmail.com>
2021-07-29 11:48:53 -0300

    Update server.js

--- server.js
+++ server.js
@@ -11,15 +11,15 @@ app.use(cors());
 if (process.env.NODE_ENV === "production") {
   // Exprees will serve up production assets
   app.use(express.static("client/build"));
-
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
 }
 
-app.use("/", express.static(resolve(__dirname, "./build")));
+// Express serve up index.html file if it doesn't recognize route
+const path = require("path");
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
+// app.use("/", express.static(resolve(__dirname, "./build")));
 
 app.listen(process.env.PORT || 3001, (err) => {
   if (err) {
